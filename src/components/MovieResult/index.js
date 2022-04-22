4637b0e525f75bda0298bbe41c69caaa72f7c2a3
Steven <stevengf14@gmail.com>
2022-04-22 12:21:23 -0500

    Modifying the style of the result

--- src/components/MovieResult/index.js
+++ src/components/MovieResult/index.js
@@ -1,20 +1,28 @@
 import React from 'react';
 import { Button, Card, Grid, Typography } from '@mui/material';
+import { withRouter } from 'react-router-dom';
+import style from './style';
 
-export default function MovieResult({ Title, Year, Type, imdbID, Poster }) {
+function MovieResult({ Title, Year, Type, imdbID, Poster, history }) {
+    const classes = style();
+
+    const handleSeeMovieClick = () => {
+        history.push(`/movie/${imdbID}`);
+    }
+
     return (
-        <Card>
+        <Card className={classes.cardContainer}>
             <Grid container>
                 <Grid item>
-                    <img src={Poster} alt={Title} />
+                    <img src={Poster} alt={Title} className={classes.poster} />
                 </Grid>
-                <Grid item>
+                <Grid item className={classes.titlesContainer}>
                     <Typography>{Title}</Typography>
                     <Typography>{Year}</Typography>
                     <Typography>{Type}</Typography>
-                    <Button color='primary' variant='contained'>more...</Button>
+                    <Button color='primary' variant='contained' onClick={handleSeeMovieClick}>Detail</Button>
                 </Grid>
             </Grid>
         </Card>
     );
-}
+}; export default withRouter(MovieResult);
---
+++ src/components/MovieResult/style.js
@@ -0,0 +1,13 @@
+import { makeStyles } from '@mui/styles';
+
+export default makeStyles({
+    cardContainer: {
+        marginBottom: 8
+    },
+    poster: {
+        width: 120
+    },
+    titlesContainer: {
+        paddingLeft: 12
+    }
+});
